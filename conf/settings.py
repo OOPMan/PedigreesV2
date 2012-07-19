b894827fa1486ea5d6ef6247a0904030c1f718c2
Adam Jorgensen <adamj@sjsoft.com>
2012-07-19 16:32:01 +0200

    Updated gender map

--- conf/settings.py
+++ conf/settings.py
@@ -3,6 +3,6 @@ __author__ = 'adamj'
 connection_string = 'postgresql+pg8000://postgres:postgres@localhost:5432/pedigrees'
 
 gender_map = {
-    1: 'MALE',
-    2: 'FEMALE'
+    'MALE': 1,
+    'FEMALE': 2
 }
